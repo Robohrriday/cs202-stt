2c6de65abe5c1bfffc8fcbe465df8b05d3703d8a
Robohrriday <robohrriday.8@gmail.com>
2025-01-09 09:28:05 +0530

    fix 2

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
+"""
+Prints Hello World! and compares two numbers.
+"""
 print("Hello World!")
 a = int(input("Enter a number: "))
 b = int(input("Enter another number: "))
 if a > b:
-                            print("3 is greater than 2")
+    print("3 is greater than 2")
